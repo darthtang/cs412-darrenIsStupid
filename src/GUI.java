749e44824d7dac288051c680d5b1d8675eec713a
Kieran Sharpe <ksharpe.2806@gmail.com>
2016-10-07 20:50:24 +0100

    GUI additions: - File -> Index now allows you to choose a folder in which to index the contents. 	(Note that it doesn't actually index them yet) - Advanced Tab has been created. No functionality yet... - Preferences Tab has been created. No content in it yet. Need to see what we want in there first.
    GUI changes to existing behaviour:
    - Made the GUI a tad more efficient. (Now only one results panel for both search tabs rather than 1 for each)
    - Some of the UI has been moved around a bit to account for the slight improvement above.

--- src/GUI.java
+++ src/GUI.java
@@ -3,6 +3,7 @@ import java.io.File;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -14,12 +15,17 @@ import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
-import javax.swing.ListSelectionModel;
 
 public class GUI {
 
 	private JFrame frmGroupProject;
 	private JTextField searchTextField;
+	private JTextField allWordsTextfield;
+	private JTextField exactWordsTextfield;
+	private JTextField anyWordsTextfield;
+	private JTextField noneWordsTextfield;
+	private JTextField numbersFromTextfield;
+	private JTextField numbersToTextfield;
 
 	/**
 	 * Launch the application.
@@ -55,6 +61,7 @@ public class GUI {
 		frmGroupProject.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmGroupProject.getContentPane().setLayout(null);
 		
+		// Start of menu items
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBounds(0, 0, 914, 22);
 		frmGroupProject.getContentPane().add(menuBar);
@@ -65,15 +72,21 @@ public class GUI {
 		JMenuItem mntmIndexFiles = new JMenuItem("Index Files");
 		mntmIndexFiles.addActionListener(e -> {
 			// TODO: add in the code to allow the user to index files
+			// JFileChooser?
+			JFileChooser chooser = new JFileChooser();
+			chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		    int returnVal = chooser.showOpenDialog(frmGroupProject);
+		    if(returnVal == JFileChooser.APPROVE_OPTION) {
+		    	// TODO: change to use the IndexFiles.java functions
+		       System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
+		    }
+			
 		});
 		mnFile.add(mntmIndexFiles);
 		
 		JMenuItem mntmQuit = new JMenuItem("Quit");
-		mntmQuit.addActionListener(e -> System.exit(0)); // quit the program
+		mntmQuit.addActionListener(e -> System.exit(0));
 		mnFile.add(mntmQuit);
-		
-		JMenu mnEdit = new JMenu("Edit");
-		menuBar.add(mnEdit);
 		
 		JMenu mnHelp = new JMenu("Help");
 		menuBar.add(mnHelp);
@@ -89,51 +102,56 @@ public class GUI {
 			// TODO: add in the code to show a popup detailing the authors of the program
 		});
 		mnHelp.add(mntmAbout);
+		// End of Menu Items
 		
+		// Start of results panel setup
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 267, 914, 290);
+		frmGroupProject.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(6, 6, 902, 243);
+		panel.add(scrollPane);
+		
+		JList<Object> list = new JList<Object>();
+		DefaultListModel<Object> dlm = new DefaultListModel<Object>();
+		list.setModel(dlm);
+		scrollPane.setViewportView(list);
+		
+		JLabel lblQuery = new JLabel("Searched for:");
+		lblQuery.setBounds(6, 261, 377, 16);
+		panel.add(lblQuery);
+		
+		JLabel lblResultsNumber = new JLabel("0 results in 0 seconds");
+		lblResultsNumber.setBounds(395, 261, 295, 16);
+		panel.add(lblResultsNumber);
+		
+		JProgressBar searchProgressBar = new JProgressBar();
+		searchProgressBar.setBounds(726, 257, 182, 20);
+		searchProgressBar.setMinimum(0);
+		searchProgressBar.setStringPainted(true);
+		panel.add(searchProgressBar);
+		// End of results panel setup
+		
+		// Start of tabbed pane setup
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(0, 21, 914, 530);
+		tabbedPane.setBounds(0, 21, 914, 246);
 		frmGroupProject.getContentPane().add(tabbedPane);
 		
+		// Start of simple tab setup
+
 		JPanel simpleTab = new JPanel();
 		tabbedPane.addTab("Simple Search", null, simpleTab, null);
 		simpleTab.setLayout(null);
 		
 		searchTextField = new JTextField();
-		searchTextField.setBounds(6, 6, 485, 26);
+		searchTextField.setBounds(6, 80, 706, 26);
 		simpleTab.add(searchTextField);
 		searchTextField.setColumns(10);
 		
-		JLabel lblQuery = new JLabel("Searched for: ");
-		lblQuery.setBounds(6, 33, 285, 16);
-		simpleTab.add(lblQuery);
-		
-		JLabel lblResultsNumber = new JLabel("0 results in 0 seconds");
-		lblResultsNumber.setBounds(6, 56, 285, 16);
-		simpleTab.add(lblResultsNumber);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(6, 84, 881, 394);
-		simpleTab.add(scrollPane);
-		
-		JList<Object> resultsList = new JList<>();
-		resultsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		DefaultListModel<Object> dlm = new DefaultListModel<>();
-		resultsList.setModel(dlm);
-		
-		scrollPane.setViewportView(resultsList);
-		
-		JLabel lblHitsInResult = new JLabel("\"Number\" of hits in \"List Selection\"");
-		lblHitsInResult.setBounds(304, 33, 316, 16);
-		simpleTab.add(lblHitsInResult);
-		
-		JProgressBar searchProgressBar = new JProgressBar();
-		searchProgressBar.setStringPainted(true);
-		searchProgressBar.setMinimum(0);
-		searchProgressBar.setBounds(700, 12, 187, 20);
-		simpleTab.add(searchProgressBar);
-		
-		JButton btnSearch = new JButton("Search");
-		btnSearch.addActionListener(e -> {
+		JButton btnSimpleSearch = new JButton("Search");
+		btnSimpleSearch.addActionListener(e -> {
 			dlm.removeAllElements();
 			Thread t = new Thread() {
 				@Override
@@ -142,6 +160,7 @@ public class GUI {
 					File root = new File("wsj-1990"); // TODO: remove hard coded value
 					String query = searchTextField.getText().trim();
 					lblQuery.setText("Searched for: " + query); // update the searched for label
+					lblResultsNumber.setText("Calculating...");
 					File[] files = root.listFiles();
 					searchProgressBar.setMaximum(files.length);
 					for (int i = 0; i < files.length; i++) {
@@ -155,19 +174,93 @@ public class GUI {
 					long endTime = System.currentTimeMillis();
 					searchProgressBar.setValue(searchProgressBar.getMaximum()); // set progress bar to complete
 					lblResultsNumber.setText(dlm.size() + " results in " + ((endTime - startTime)/1000) + " seconds"); // update the number of hits label
-					
 				}
 			};
 			t.start(); // start the thread to search the files
 		});
-		btnSearch.setBounds(503, 6, 117, 29);
-		simpleTab.add(btnSearch);
+		btnSimpleSearch.setBounds(713, 80, 174, 29);
+		simpleTab.add(btnSimpleSearch);
+		// End of simple tab setup
 		
-		JLabel lblSearchProgress = new JLabel("Progress:");
-		lblSearchProgress.setBounds(632, 11, 68, 16);
-		simpleTab.add(lblSearchProgress);
-		
+		// Start of advanced tab setup
 		JPanel advancedTab = new JPanel();
 		tabbedPane.addTab("Advanced Search", null, advancedTab, null);
+		advancedTab.setLayout(null);
+		
+		JLabel lblFindDocumentsWith = new JLabel("Find documents with...");
+		lblFindDocumentsWith.setBounds(6, 6, 155, 16);
+		advancedTab.add(lblFindDocumentsWith);
+		
+		JLabel lblAllTheseWords = new JLabel("all these words:");
+		lblAllTheseWords.setBounds(6, 28, 124, 16);
+		advancedTab.add(lblAllTheseWords);
+		
+		JLabel lblThisExactWord = new JLabel("this exact word or phrase:");
+		lblThisExactWord.setBounds(6, 56, 170, 16);
+		advancedTab.add(lblThisExactWord);
+		
+		JLabel lblAnyOfThese = new JLabel("any of these words:");
+		lblAnyOfThese.setBounds(6, 84, 137, 16);
+		advancedTab.add(lblAnyOfThese);
+		
+		allWordsTextfield = new JTextField();
+		allWordsTextfield.setBounds(188, 23, 699, 26);
+		advancedTab.add(allWordsTextfield);
+		allWordsTextfield.setColumns(10);
+		
+		exactWordsTextfield = new JTextField();
+		exactWordsTextfield.setBounds(188, 51, 699, 26);
+		advancedTab.add(exactWordsTextfield);
+		exactWordsTextfield.setColumns(10);
+		
+		anyWordsTextfield = new JTextField();
+		anyWordsTextfield.setBounds(188, 79, 699, 26);
+		advancedTab.add(anyWordsTextfield);
+		anyWordsTextfield.setColumns(10);
+		
+		JLabel lblNoneOfThese = new JLabel("none of these words:");
+		lblNoneOfThese.setBounds(6, 112, 155, 16);
+		advancedTab.add(lblNoneOfThese);
+		
+		noneWordsTextfield = new JTextField();
+		noneWordsTextfield.setBounds(188, 107, 699, 26);
+		advancedTab.add(noneWordsTextfield);
+		noneWordsTextfield.setColumns(10);
+		
+		JLabel lblNumbersRangingFrom = new JLabel("numbers ranging from:");
+		lblNumbersRangingFrom.setBounds(6, 140, 164, 16);
+		advancedTab.add(lblNumbersRangingFrom);
+		
+		numbersFromTextfield = new JTextField();
+		numbersFromTextfield.setBounds(188, 135, 301, 26);
+		advancedTab.add(numbersFromTextfield);
+		numbersFromTextfield.setColumns(10);
+		
+		JLabel lblTo = new JLabel("to");
+		lblTo.setBounds(501, 140, 33, 16);
+		advancedTab.add(lblTo);
+		
+		numbersToTextfield = new JTextField();
+		numbersToTextfield.setBounds(546, 135, 341, 26);
+		advancedTab.add(numbersToTextfield);
+		numbersToTextfield.setColumns(10);
+		
+		JButton btnAdvancedSearch = new JButton("Search");
+		btnAdvancedSearch.setBounds(0, 168, 117, 29);
+		btnAdvancedSearch.addActionListener(e -> {
+			// TODO: add in the logic for an advanced search
+		});
+		advancedTab.add(btnAdvancedSearch);
+		// End of advanced tab setup
+
+		// Start of preferences tab setup
+		JPanel preferencesTab = new JPanel();
+		tabbedPane.addTab("Preferences", null, preferencesTab, null);
+		preferencesTab.setLayout(null);
+		
+		JLabel lblTempPrefTabLabel = new JLabel("To be completed, duh...");
+		lblTempPrefTabLabel.setBounds(365, 70, 176, 25);
+		preferencesTab.add(lblTempPrefTabLabel);
+		// End of preferences tab setup
 	}
 }
