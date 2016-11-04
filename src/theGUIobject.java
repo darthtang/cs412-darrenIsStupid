f8f7b6ed12b1ae2c55dbeeefda4d2895d5bce92b
Richard Tang <pjb12147@ds.strath.ac.uk>
2016-11-04 13:57:02 +0000

    start of a new object that K will get and use to display stuff in GUI

---
+++ src/theGUIobject.java
@@ -0,0 +1,14 @@
+
+public class theGUIobject {
+
+
+	    public String path;
+	    public String hitwords;
+	    //constructor
+	    public theGUIobject(String pathinput, String hitwordsinput) {
+	        path = pathinput;
+	        hitwords = hitwordsinput;
+	    }
+	
+	
+}
