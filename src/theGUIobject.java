736db5bab71de2ef63a9ddbdda78f3b956eec6a5
Richard Tang <pjb12147@ds.strath.ac.uk>
2016-11-04 14:00:49 +0000

    more stuff to the object so omer can pass to K

--- src/theGUIobject.java
+++ src/theGUIobject.java
@@ -4,10 +4,17 @@ public class theGUIobject {
 
 	    public String path;
 	    public String hitwords;
+	    public String titleOfXML;
+	    public String firstTenWordsOfTextTag;
+	    
+	    
 	    //constructor
-	    public theGUIobject(String pathinput, String hitwordsinput) {
+	    public theGUIobject(String pathinput, String hitwordsinput, String XMLTitle, String tenWords) {
 	        path = pathinput;
 	        hitwords = hitwordsinput;
+	        titleOfXML = XMLTitle;
+	        firstTenWordsOfTextTag = tenWords;
+	        
 	    }
 	
 	
